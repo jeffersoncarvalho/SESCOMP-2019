c873c2dc88d8d75b1647e717a09232ee745103d0
jeffersoncarvalho <jeffersoncarvalho@gmail.com>
2019-10-09 11:06:43 -0300

    desenhando heroi - aula 03

--- jogo.py
+++ jogo.py
@@ -9,6 +9,9 @@ pygame.init()
 display_width = 640
 display_height = 480
 
+#cores
+branca = (255,255,255)
+
 #setup
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 gameDisplay.fill((255,255,255))
@@ -20,6 +23,11 @@ pygame.display.set_caption("SESCOMP GAME")
 #clock
 clock = pygame.time.Clock()
 
+#dados do heroi
+heroi = pygame.image.load("emoji.png")
+hx = 50
+hy = 150
+
 ####LOGICA PRINCIPAL
 fim = False
 #loop do jogo
@@ -30,6 +38,11 @@ while not fim:
 			fim = True
 		#imprimindo evento
 		print(evento)
+    #apaga a tela
+	gameDisplay.fill(branca)
+    #desenha o heroi
+	gameDisplay.blit(heroi,(hx,hy))
+    #atualiza tela
 	pygame.display.update()
 	clock.tick(60)
 
